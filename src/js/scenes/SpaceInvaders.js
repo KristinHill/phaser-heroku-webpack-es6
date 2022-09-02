260c3d394630cf22a3d376135ddb52df97214a82
Kristin Hill <krstinhill4211@gmail.com>
2022-09-02 16:24:52 -0700

    pls work now

--- src/js/scenes/SpaceInvaders.js
+++ src/js/scenes/SpaceInvaders.js
@@ -65,7 +65,7 @@ export default class SpaceInvaders extends Phaser.Scene {
       this.load.image('redLaser', '.../public/assets/images/redLaser.png');
       this.load.image('greenLaser', '.../public/assets/images/greenLaser.png');
       this.load.audio("laserBad", [".../public/assets/audio/laser3.mp3", ".../public/assets/audio/laser3.ogg"]);
-      this.load.audio("game", [".../public/assets/audio/Night on the Ice.mp3"]);
+      this.load.audio("game", [".../public/assets/audio/Night-on-the-Ice.mp3"]);
       this.load.audio("flyingShip", [".../public/assets/audio/shipFly.wav"]);
       this.load.audio("boom", [".../public/assets/audio/boom.wav"]);
       this.load.audio("laserGood", [".../public/assets/audio/laser5.mp3", ".../public/assets/audio/laser5.ogg"]);
